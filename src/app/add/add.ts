4b5594277b28f1ee991e8c9aa27e4d2b996e2803
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-30 13:05:23 +0200

    funktion för att spara data med POST

--- src/app/add/add.ts
+++ src/app/add/add.ts
@@ -1,29 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-add',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './add.html',
   styleUrl: './add.css',
 })
 export class Add implements OnInit {
-  //Sparar jobben från databasen
-  workExperiences: any [] = [];
+  // Behållaren för listan
+  workExperiences: any[] = [];
+
+  // Behållaren för det nya som skrivs in
+  newExperience = {
+    companyname: '',
+    jobtitle: '',
+    location: '',
+    description: ''
+  };
+
   ngOnInit() {
     this.getData();
+  }
 
-    //Hämta data från min databas
-    async getLocaleDateFormat() {
-      try {
-        const res = await fetch("http://localhost:5001/workexperience")
-        if (!res.ok) {
-          throw new Error("Kunde inte hämta data");
-        }
+  // Hämta data
+  async getData() {
+    try {
+      const res = await fetch("http://localhost:5001/workexperience");
+      if (res.ok) {
         this.workExperiences = await res.json();
-      } catch (error) {
-        console.error("Fel vid hämtning:", error);
       }
+    } catch (error) {
+      console.error("Kunde inte hämta", error);
     }
   }
-}
+
+  // SPARA DATA 
+  async addExperience() {
+    try {
+      const res = await fetch("http://localhost:5001/workexperience", { 
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.newExperience)
+      });
+
+      if (res.ok) {
+        // Töm formuläret
+        this.newExperience = { companyname: '', jobtitle: '', location: '', description: '' };
+        // Uppdatera listan
+        this.getData();
+      }
+    } catch (error) {
+      console.error("Kunde inte spara", error);
+    }
+  }
+} 
