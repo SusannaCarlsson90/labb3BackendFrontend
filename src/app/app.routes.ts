c10097322661b297b41c38e3ce846a877cc9ebfe
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-30 11:16:12 +0200

    lagt till komponeneter, lite styling samt klistrat in html från labb2 för formuläret under add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';        
+import { About } from './about/about';       
+import { Add } from './add/add';             
+import { Notfound } from './notfound/notfound'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "home", component: Home },         
+  { path: "about", component: About },     
+  { path: "add", component: Add },           
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "404", component: Notfound },      
+  { path: "**", redirectTo: "404", pathMatch: "full" }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from './nav/nav'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  standalone: true,
+  imports: [RouterOutlet, Nav],
+  templateUrl: './app.html',      
+  styleUrl: './app.css'       
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class AppComponent { }
