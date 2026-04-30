f6aa348b1bed6cb39c12074112c61e33d945dac2
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-30 11:29:41 +0200

    Hämtning av API i Add.ts

--- src/app/add/add.ts
+++ src/app/add/add.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add.html',
   styleUrl: './add.css',
 })
-export class Add {}
+export class Add implements OnInit {
+  //Sparar jobben från databasen
+  workExperiences: any [] = [];
+  ngOnInit() {
+    this.getData();
+
+    //Hämta data från min databas
+    async getLocaleDateFormat() {
+      try {
+        const res = await fetch("http://localhost:5001/workexperience")
+        if (!res.ok) {
+          throw new Error("Kunde inte hämta data");
+        }
+        this.workExperiences = await res.json();
+      } catch (error) {
+        console.error("Fel vid hämtning:", error);
+      }
+    }
+  }
+}
